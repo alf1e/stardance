cc976a50da3ac2f79754122d1a207737e73cebd4
Max Wofford <max@maxwofford.com>
2026-05-10 12:27:04 -0400

    fix(flipper): silence "Could not find feature" warnings on fresh db

--- config/initializers/flipper.rb
+++ config/initializers/flipper.rb
@@ -18,20 +18,24 @@ Rails.application.config.after_initialize do
     next unless ActiveRecord::Base.connection.table_exists?(:flipper_features)
 
     # Feature flags used throughout the codebase
-    Flipper.add(:shop_open) unless Flipper.exist?(:shop_open)
-    Flipper.add(:"git_commit_2025-12-25") unless Flipper.exist?(:"git_commit_2025-12-25")
-    Flipper.add(:shop_suggestion_box) unless Flipper.exist?(:shop_suggestion_box)
-    Flipper.add(:voting) unless Flipper.exist?(:voting)
-    Flipper.add(:admin_dark) unless Flipper.exist?(:admin_dark)
-    Flipper.add(:shop_backlogged) unless Flipper.exist?(:shop_backlogged)
-    Flipper.add(:kitchen_comic) unless Flipper.exist?(:kitchen_comic)
-    Flipper.add(:admin_dark_brown_buttons) unless Flipper.exist?(:admin_dark_brown_buttons)
-    Flipper.add(:grant_stardust) unless Flipper.exist?(:grant_stardust)
-    Flipper.add(:fraud_daily_summary) unless Flipper.exist?(:fraud_daily_summary)
-    Flipper.add(:shop_order_daily_summary) unless Flipper.exist?(:shop_order_daily_summary)
-    Flipper.add(:shipping) unless Flipper.exist?(:shipping)
-    Flipper.add(:show_and_tell_live) unless Flipper.exist?(:show_and_tell_live)
-    Flipper.add(:missions) unless Flipper.exist?(:missions)
+    Flipper::Adapters::Strict.with_sync_mode do
+      %w[
+        shop_open
+        git_commit_2025-12-25
+        shop_suggestion_box
+        voting
+        admin_dark
+        shop_backlogged
+        kitchen_comic
+        admin_dark_brown_buttons
+        grant_stardust
+        fraud_daily_summary
+        shop_order_daily_summary
+        shipping
+        show_and_tell_live
+        missions
+      ].each { |flag| Flipper.add(flag) }
+    end
   rescue StandardError => e
     Rails.logger.warn "Could not initialize flipper: #{e.message}"
   end
